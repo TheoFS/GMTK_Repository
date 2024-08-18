fba013d8d3c2e757f6dbb2816eb44911721b7649
TheoFS <80713274+TheoFS@users.noreply.github.com>
2024-08-18 18:38:36 -0300

    Gráficos do Cachorro

---
+++ SalsichaGame/Assets/Scripts/Torso.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torso : MonoBehaviour
+{
+    public Vector3 direction;
+    public SpriteRenderer spriteRenderer;
+    /*SpriteRenderer torsoSpriteRenderer;
+
+    public Vector3
+
+    private void Start()
+    {
+        torsoSpriteRenderer.GetComponent<SpriteRenderer>();
+    }*/
+}
